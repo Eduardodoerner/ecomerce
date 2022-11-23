a85576bf379a78400915293e59303cd9f5adc31d
Eduardodoerner <110205124+Eduardodoerner@users.noreply.github.com>
2022-11-23 13:09:20 -0300

    att6

--- menu.php
+++ menu.php
@@ -7,6 +7,6 @@ $resultado = $consulta->execute();
 while ($linha_menu = $consulta->fetch()) {
     echo "<a href=\"?pagina={$linha_menu['url']}\">{$linha_menu['label']}</a> - ";
 }
-?>
+?> 
 <a href="?pagina=logout">Sair</a>
 <hr>
