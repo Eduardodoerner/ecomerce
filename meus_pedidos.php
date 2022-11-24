fe15da91a0e0cf9f505789fb57a52f769e070e50
Eduardodoerner <110205124+Eduardodoerner@users.noreply.github.com>
2022-11-24 20:54:11 -0300

    Update meus_pedidos.php

--- meus_pedidos.php
+++ meus_pedidos.php
@@ -1,14 +1,10 @@
-<h3>Meus Pedidos: <?php echo $_SESSION['usuario']['login']; ?></h3>
-
+<h3>Seus Pedidos: <?php echo $_SESSION['usuario']['login']; ?></h3>
 <?php
 $sql_pedidos = 'select * from vendas where usuario_id = :usuario_id';
 $sql_pedidos = $conn->prepare($sql_pedidos);
 $sql_pedidos->execute(['usuario_id' => $_SESSION['usuario']['id']]);
-
-while ($linha = $sql_pedidos->fetch()) { 
-
+while ($linha = $sql_pedidos->fetch()) {
     echo "<h5>#{$linha['id']} - {$linha['data_venda']}</h5>";
-
     $sql_itens = "
         select * 
         from vendas_produtos v
@@ -17,14 +13,14 @@ while ($linha = $sql_pedidos->fetch()) {
     ";
     $sql_itens = $conn->prepare($sql_itens);
     $sql_itens->execute(['venda_id' => $linha['id']]);
-    ?>
+ ?>
 <table class="table">
     <thead>
         <tr>
-            <th scope="col">#ID</th>
-            <th scope="col">Produto ID</th>
-            <th scope="col">Descricao</th>
-            <th scope="col">Valor</th>
+        <th scope="col">#ID</th>
+        <th scope="col">Produto ID</th>
+        <th scope="col">Descricao</th>
+        <th scope="col">Valor</th>
         </tr>
     </thead>
     <tbody>
@@ -33,24 +29,19 @@ while ($linha = $sql_pedidos->fetch()) {
         while ($item = $sql_itens->fetch()) {
             $total += $item['valor']; ?>
         <tr>
-            <th scope="row"><?php echo $item['id']; ?></th>
-            <td><?php echo $item['produto_id']; ?></td>
-            <td><?php echo $item['descricao']; ?></td>
-            <td><?php echo $item['valor']; ?></td>
+         <th scope="row"><?php echo $item['id']; ?></th>
+         <td><?php echo $item['produto_id']; ?></td>
+         <td><?php echo $item['descricao']; ?></td>
+         <td><?php echo $item['valor']; ?></td>
         </tr>
         <?php
         }
         ?>
         <tr>
-            <td>
-                Total: <?php echo $total; ?>
-            </td>
+         <td>
+         Total: <?php echo $total; ?>
+         </td>
         </tr>
     </tbody>
 </table>
 <br>
-<?php
-}
-
-
-?>
