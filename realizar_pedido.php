f85fee0b1a2675aec7f3474f8ae6e89c37e01321
Eduardodoerner <110205124+Eduardodoerner@users.noreply.github.com>
2022-11-24 20:58:35 -0300

    Update realizar_pedido.php

--- realizar_pedido.php
+++ realizar_pedido.php
@@ -1,19 +1,16 @@
 <h1>
     terminar pedido.
 </h1>
-
 <?php if (isset($_POST['gravar_pedido'])) {
     $sql_insere_venda = 'INSERT into vendas (usuario_id,data_venda) values (:id, now())';
     $sql_insere_venda = $conn->prepare($sql_insere_venda);
     $sql_insere_venda->execute(['id' => $_SESSION['usuario']['id']]);
-
     $venda_id = $conn->lastInsertId();
     foreach ($_SESSION['sacola'] as $item) {
         $sql_insert_item = "
             INSERT into vendas_produtos
             (venda_id, produto_id, valor_venda)
-            values(:venda_id, :produto_id, (select valor from produtos where id = :produto_id ))
-        ";
+            values(:venda_id, :produto_id, (select valor from produtos where id = :produto_id ))";
         $sql_insert_item = $conn->prepare($sql_insert_item);
         $sql_insert_item->execute(['venda_id' => $venda_id, 'produto_id' => $item[0]]);
     }
@@ -23,11 +20,8 @@
     unset($_SESSION['sacola']);
 } else {
      ?>
-
 <form method="post">
     <input class="btn btn-success" type="submit" name="gravar_pedido" value="Confirmar!">
 </form>
-
 <?php
-}
-?>
+}?>
